c5db80c0fbe811146355ce30a07b28eeff004959
SFL-Master <48449300+SFL-Master@users.noreply.github.com>
2019-04-21 23:35:16 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,15 +1,25 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-description 'ESX Nightclub Job'
+description 'ESX Nightclub Job & Addons'
 
-version '1.0.0'
+version '1.1.0'
 
 client_scripts {
+  --JOB-- 
   '@es_extended/locale.lua',
   'locales/en.lua',
   'locales/de.lua',
   'config.lua',
-  'client/main.lua'
+  'client/main.lua',
+  --------------------------
+  --Extras--
+  'scripts/nightclub.lua',
+  'scripts/screens.lua',
+  'scripts/teleport.lua',
+  'scripts/peds.lua',
+  ---------------------------
+  
+  
 }
 
 server_scripts {
@@ -17,5 +27,5 @@ server_scripts {
   'locales/en.lua',
   'locales/de.lua',
   'config.lua',
-  'server/main.lua'
+  'server/main.lua',
 }
